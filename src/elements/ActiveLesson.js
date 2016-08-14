3a10942004e34ca500c71176acbba202d154c40b
padster <padsterpat@gmail.com>
2016-08-13 23:07:30 -0700

    Hooked up to redux

--- src/elements/ActiveLesson.js
+++ src/elements/ActiveLesson.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+import Store from '../Store.js';
+
 class ActiveLesson extends Component {
+  storeListener: null;
+
+  // HACK - should observe store values instead :(
+  componentDidMount() {
+    this.storeListener = Store.subscribe(() => this.forceUpdate());
+  }
+  componentWillUnmount() {
+    if (this.storeListener) {
+      this.storeListener();
+    }
+  }
+
   render() {
+    const lesson = Store.getState().activeLesson;
     return (
       <div className="activeLesson">
-        Lesson goes here...
+        {lesson == null ? this.renderNoLesson() : this.renderLesson(lesson)}
+      </div>
+    );
+  }
+
+  renderNoLesson() {
+    return "Pick a lesson...";
+  }
+
+  renderLesson(lesson) {
+    return (
+      <div>
+        Lesson: {lesson.name}
       </div>
     );
   }
--- src/elements/LessonList.js
+++ src/elements/LessonList.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
+import Store from '../Store.js';
+
+// TODO - migrate somewhere better.
+const LESSONS = [{
+  name: 'trial',
+  config: {
+    notes: 2,
+  },
+}];
+
 class LessonList extends Component {
   render() {
     return (
       <div className="lessonList">
-        Lessons go here...
+        <div className="listHolder">
+          {LESSONS.map((lesson, i) => {
+            const handler = () => this.pickLesson(lesson);
+            return (
+              <div key={i} className="listItem" onClick={handler}>
+                {lesson.name}
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
+  }
+
+  pickLesson(lesson) {
+    Store.dispatch({
+      type: 'SET_ACTIVE_LESSON', lesson,
+    });
   }
 }
 
